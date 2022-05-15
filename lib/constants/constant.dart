1a091cb71ba73581c9b8920038f5fcb7c7eb58ee
HumaCode <humaidizakaria1998@gmail.com>
2022-05-15 14:48:57 +0700

    update 1

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -12,3 +12,13 @@ TextStyle numberTextStyle =
 
 TextStyle result =
     TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.white);
+
+// kategori
+const String underWeightSevere = "Berat Badan Kurang (Parah)";
+const String underWeightModerete = "Berat Badan Kurang (Sedang)";
+const String underWeightMild = "Berat Badan Kurang (Ringan)";
+const String normal = "Normal";
+const String overweight = "Kegemukan";
+const String obeseI = "Obesitas (Kelas I)";
+const String obeseII = "Obesitas (Kelas II)";
+const String obeseIII = "Obesitas (Kelas III)";
--- lib/views/bmi_result_screen.dart
+++ lib/views/bmi_result_screen.dart
@@ -7,8 +7,64 @@ class BmiResultScreen extends StatelessWidget {
 
   final double bmi;
 
+// kategori
+  String determineBmiCategory(double bmiValue) {
+    String category = "";
+    if (bmiValue < 16.0) {
+      category = underWeightSevere;
+    } else if (bmiValue < 17) {
+      category = underWeightModerete;
+    } else if (bmiValue < 18.5) {
+      category = underWeightMild;
+    } else if (bmiValue < 25) {
+      category = normal;
+    } else if (bmiValue < 30) {
+      category = overweight;
+    } else if (bmiValue < 35) {
+      category = obeseI;
+    } else if (bmiValue < 40) {
+      category = obeseII;
+    } else if (bmiValue >= 40) {
+      category = obeseIII;
+    }
+
+    return category;
+  }
+
+  // Keterangan Kategori
+  String getHealthRiskDescription(String categoryName) {
+    String desc = "";
+
+    switch (categoryName) {
+      case underWeightSevere:
+      case underWeightModerete:
+      case underWeightMild:
+        desc = "Kemungkinan kekurangan nutrisi dan pengkroposan tulang";
+        break;
+      case normal:
+        desc = "Resiko Rendah(Sehat)";
+        break;
+      case overweight:
+        desc =
+            "Risiko terkena penyakit jantung, tekanan darah tinggi, stroke dan diabetes militus";
+        break;
+      case obeseI:
+      case obeseII:
+      case obeseIII:
+        desc =
+            "Risiko tinggi terkena penyakit jantung, tekanan darah tinggi, stroke, diabetes mellitus, sindrom metabolisme";
+        break;
+      default:
+    }
+
+    return desc;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final bmiCategory = determineBmiCategory(bmi);
+    final bmiDesc = getHealthRiskDescription(bmiCategory);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Hasil Hitung "),
@@ -28,17 +84,19 @@ class BmiResultScreen extends StatelessWidget {
           Expanded(
             flex: 5,
             child: BmiCard(
-              child: Padding(
+              child: Container(
+                width: double.infinity,
                 padding: const EdgeInsets.symmetric(horizontal: 15.0),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Text(
-                      "Normal",
+                      bmiCategory,
                       style: result.copyWith(
                         fontSize: 25,
                         fontWeight: FontWeight.normal,
                       ),
+                      textAlign: TextAlign.center,
                     ),
                     Text(
                       "${bmi.toStringAsFixed(1)}",
@@ -47,7 +105,7 @@ class BmiResultScreen extends StatelessWidget {
                       ),
                     ),
                     Text(
-                      "Kamu terlalu kurus, perbanyak makan.",
+                      bmiDesc,
                       style: result.copyWith(
                         fontSize: 25,
                         fontWeight: FontWeight.normal,
